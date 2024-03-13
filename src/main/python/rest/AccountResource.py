231fac4caf8a3fb965240bfedad13d6ca1397b60
Daniela <daniela.adv.34@gmail.com>
2024-03-13 23:44:08 +0100

    PIB-55: Atualização do backend - Usuário

--- src/main/python/rest/AccountResource.py
+++ src/main/python/rest/AccountResource.py
@@ -43,18 +43,18 @@ managed_user_schema = ManagedUserSchema()
 
 class ManagedUserAccountRegister(Resource):
     def post(self):
-        logging.info("POST request received on ManagedUserRegister")
+        logging.info("Recebida solicitação POST em ManagedUserRegister")
         current_login = "system"
         managed_user = request.get_json()
         # Check that the username and email are not already used
         user_name = User.get_by_login(managed_user["login"])
         if user_name is not None:
-            return {"message": "Username is already in use"}, 400
+            return {"message": "O nome de usuário já está em uso"}, 400
         user_email = User.get_by_email(managed_user["email"])
         if user_email is not None:
-            return {"message": "Email is already in use"}, 400
+            return {"message": "O email já está em uso"}, 400
         if not is_password_length_valid(managed_user["password"]):
-            return {"message": "Invalid password length"}, 400
+            return {"message": "Comprimento de senha inválido"}, 400
         # Salt the password and then encrypt it using bcrypt
         managed_user["password"] = encrypt_password(managed_user["password"]).decode('utf-8')
         # Newly registered users should get a ROLE_USER role by default
@@ -67,7 +67,7 @@ class ManagedUserAccountRegister(Resource):
             return {"message": json.dumps(err.messages)}, 400
         # Add activation key, created date, created by login to the entry and add it
         if managed_user_data.login is None:
-            return {"message": "Unable to register"}, 400
+            return {"message": "Não foi possível registrar"}, 400
         reset_key = generate_activation_reset_keys()
         managed_user_data.set_reset_key(reset_key)
         managed_user_data.set_created_date(datetime.now(timezone.utc))
@@ -81,8 +81,8 @@ class ManagedUserAccountRegister(Resource):
         try:
             send_creation_mail(managed_user_data)
         except:
-            return {"message": "User is created but unable to send mail"}, 200
-        return {"message": "Successfully registered"}, 201
+            return {"message": "Usuário foi criado, mas não foi possível enviar o email"}, 200
+        return {"message": "Registro realizado com sucesso"}, 201
 
 
 account_ns = Namespace('account-resource', path="/account")
@@ -93,21 +93,21 @@ admin_user_schema = AdminUserSchema()
 class AdminAccountDetails(Resource):
     @jwt_required()
     def post(self):
-        logging.info("POST request received on AdminAccountDetails")
+        logging.info("Requisição POST recebida em AdminAccountDetails")
         login = get_jwt_identity()
         user_json = request.get_json()
         if user_json["id"] is None:
-            return {"message": "No User found"}, 400
+            return {"message": "Nenhum usuário encontrado"}, 400
         current_user = User.get_by_login(login)
         if current_user is None:
-            return {"message": "User not found"}, 404
+            return {"message": "Usuário não encontrado"}, 404
         if current_user.id != user_json["id"]:
-            return {"message": "Invalid User"}, 400
+            return {"message": "Usuário inválido"}, 400
         # Before updating check that the email is not already used by any other user
         user_email = User.get_by_email(user_json["email"])
         if user_email is not None:
             if user_email.id != user_json["id"]:
-                return {"message": "Email is already in use"}, 400
+                return {"message": "O email já está em uso"}, 400
         # TODO: Should update only first name, last name, email, language
         try:
             updated_user = admin_user_schema.load(user_json, instance=current_user)
@@ -124,7 +124,7 @@ class AdminAccountDetails(Resource):
 
     @jwt_required(optional=True)
     def get(self):
-        logging.info("GET request received on AdminAccountDetails")
+        logging.info("Requisição GET recebida em AdminAccountDetails")
         login = get_jwt_identity()
         if login is None:
 
@@ -134,7 +134,7 @@ class AdminAccountDetails(Resource):
 
         current_user = User.get_by_login(login)
         if current_user is None:
-            return {"message": "User could not be found"}, 500
+            return {"message": "Usuário não pôde ser encontrado"}, 500
         return admin_user_schema.dump(current_user), 200
 
 
@@ -147,7 +147,7 @@ class AccountAuthenticate(Resource):
         logging.info("GET request received on AccountAuthenticate")
         login = get_jwt_identity()
         if login is None:
-            return {"message": "No authenticated User not found"}, 404
+            return {"message": "Usuário autenticado não encontrado"}, 404
         return login, 200
 
 
@@ -156,12 +156,12 @@ account_activate_ns = Namespace('account-resource', path="/activate")
 
 class AccountActivate(Resource):
     def get(self):
-        logging.info("GET request received on AccountActivate")
+        logging.info("Requisição GET recebida em AccountActivate")
         activation_key = request.args.get("key")
-        # Get authentication key and search for user with that key
+        # Obter chave de autenticação e procurar usuário com essa chave
         user_data = User.get_by_activation_key(activation_key)
         if user_data is None:
-            return {"message": "No User found for given activation key"}, 400
+            return {"message": "Nenhum usuário encontrado para a chave de ativação fornecida"}, 400
         # Mark the user as active
         user_data.set_activated(True)
         user_data.set_activation_key(None)
@@ -175,8 +175,8 @@ class AccountActivate(Resource):
         try:
             send_activation_mail(user_data)
         except:
-            return {"message": "User is activated but unable to send mail"}, 400
-        return {"message": "Account activated"}, 200
+            return {"message": "Usuário está ativado, mas não foi possível enviar o email"}, 400
+        return {"message": "Conta ativada"}, 200
 
 
 passwd_reset_init_ns = Namespace('account-resource', path="/account/reset-password/init")
@@ -184,13 +184,13 @@ passwd_reset_init_ns = Namespace('account-resource', path="/account/reset-passwo
 
 class AccountPasswordResetInit(Resource):
     def post(self):
-        logging.info("POST request received on AccountPasswordResetInit")
+        logging.info("Requisição POST recebida em AccountPasswordResetInit")
         password_reset_mail = request.get_data().decode('utf-8')
         print(password_reset_mail)
         user = User.get_by_email(password_reset_mail)
         if user is None:
-            logging.info("No user found for the supplied email address: " + password_reset_mail)
-            return {"message": "No user found for this email address"}, 400
+            logging.info("Nenhum usuário encontrado para o email fornecido: " + password_reset_mail)
+            return {"message": "Nenhum usuário encontrado para este endereço de email"}, 400
         reset_key = generate_activation_reset_keys()
         user.set_reset_key(reset_key)
         user.set_reset_date(datetime.now(timezone.utc))
@@ -202,8 +202,8 @@ class AccountPasswordResetInit(Resource):
         try:
             send_reset_mail(user)
         except:
-            return {"message": "Password reset request initiated but unable to send mail"}, 400
-        return {"message": "Password reset request submitted"}, 200
+            return {"message": "Solicitação de redefinição de senha iniciada, mas não foi possível enviar o email"}, 400
+        return {"message": "Solicitação de redefinição de senha enviada"}, 200
 
 
 passwd_reset_finish_ns = Namespace('account-resource', path="/account/reset-password/finish")
@@ -212,13 +212,13 @@ key_password_schema = KeyAndPasswordSchema()
 
 class AccountPasswordResetFinish(Resource):
     def post(self):
-        logging.info("POST request received on AccountPasswordResetFinish")
+        logging.info("Requisição POST recebida em AccountPasswordResetFinish")
         password_reset_finish = request.get_json()
         if not is_password_length_valid(password_reset_finish["newPassword"]):
-            return {"message": "Invalid password length"}, 400
+            return {"message": "Comprimento de senha inválido"}, 400
         user = User.get_by_reset_key(password_reset_finish["key"])
         if user is None:
-            return {"message": "No User found for given reset key"}, 400
+            return {"message": "Nenhum usuário encontrado para a chave de reset fornecida"}, 400
         # Salt and encrypt the password
         new_password = encrypt_password(password_reset_finish["newPassword"]).decode('utf-8')
         user.set_reset_key(None)
@@ -231,7 +231,7 @@ class AccountPasswordResetFinish(Resource):
             user.save_to_db()
         except SQLAlchemyError as e:
             return {"message": str(e.__dict__['orig'])}, 400
-        return {"message": "Password reset successful"}, 200
+        return {"message": "Redefinição de senha bem-sucedida"}, 200
 
 
 change_passwd_ns = Namespace('account-resource', path="/account/change-password")
@@ -240,19 +240,19 @@ change_passwd_ns = Namespace('account-resource', path="/account/change-password"
 class AccountChangePassword(Resource):
     @jwt_required()
     def post(self):
-        logging.info("POST request received on AccountChangePassword")
+        logging.info("Recebida solicitação POST em AccountChangePassword")
         login = get_jwt_identity()
         current_user = User.get_by_login(login)
         if current_user is None:
-            return {"message": "Current logged in User not found"}, 404
+            return {"message": "Usuário logado atual não encontrado"}, 404
         password_reset_finish = request.get_json()
         old_password = password_reset_finish["currentPassword"]
         new_password = password_reset_finish["newPassword"]
         if not is_password_length_valid(new_password):
-            return {"message": "Invalid password length"}, 400
-        # Check the existing password
+            return {"message": "Comprimento de senha inválido"}, 400
+        # Verifique a senha existente
         if bcrypt.checkpw(old_password.encode('utf8'), current_user.password_hash.encode('utf8')) is not True:
-            return {"message": "Invalid existing password supplied"}, 401
+            return {"message": "Senha existente fornecida inválida"}, 401
         # Salt and encrypt the password before storing
         new_password = encrypt_password(new_password).decode('utf-8')
         current_user.set_password(new_password)
@@ -262,4 +262,4 @@ class AccountChangePassword(Resource):
             current_user.save_to_db()
         except SQLAlchemyError as e:
             return {"message": str(e.__dict__['orig'])}, 400
-        return {"message": "Password reset successfully"}, 200
+        return {"message": "Redefinição de senha realizada com sucesso"}, 200
